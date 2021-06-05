1fc67eac5829c8171c1eee0a94ebc90948c3ddb1
jill.tu <jilltu330@gmail.com>
2021-06-06 00:20:35 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 //required packages used in this project
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
+
+mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 //required express-handlebars here
 const exphbs = require('express-handlebars')
