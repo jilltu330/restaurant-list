fae75a3c616d29b8101c2f47dfc9e90a7734dd4e
jill.tu <jilltu330@gmail.com>
2021-06-06 14:08:12 +0800

    feat: add update restaurant function

--- app.js
+++ app.js
@@ -69,6 +69,34 @@ app.get('/restaurants/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
+//edit route
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('edit', { restaurant }))
+    .catch(error => console.log(error))
+})
+
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => {
+      restaurant.name = req.body.name,
+        restaurant.name_en = req.body.name_en,
+        restaurant.category = req.body.category,
+        restaurant.image = req.body.image,
+        restaurant.location = req.body.location,
+        restaurant.phone = req.body.phone,
+        restaurant.google_map = req.body.google_map,
+        restaurant.rating = req.body.rating,
+        restaurant.description = req.body.description,
+        restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(error => console.log(error))
+})
+
 //search route
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword.trim()
