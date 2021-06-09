70e86b27e198c99c82df116d7409d42e7e0e6000
jill.tu <jilltu330@gmail.com>
2021-06-09 18:15:21 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,31 +1,16 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 
 const routes = require('./routes')
+require('./config/mongoose')
+
 const app = express()
 const port = 3000
 
-mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
-//setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-
-//setting static files
 app.use(express.static('public'))
-
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
