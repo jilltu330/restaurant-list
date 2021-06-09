8f88bb80ca2edbb09e467905befa21ae293ec914
jill.tu <jilltu330@gmail.com>
2021-06-09 22:28:27 +0800

    fix routing issue

--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -3,9 +3,38 @@ const router = express.Router()
 
 const Restaurant = require('../../models/restaurant')
 
-//Create
+//define restaurant route
 router.get('/new', (req, res) => {
   return res.render('new')
+})
+
+//Search
+router.get('/search', (req, res) => {
+  const keyword = req.query.keyword.trim()
+  const noResultNotice = `你搜尋的${keyword}沒有符合的餐廳`
+  const emptySearchNotice = '請輸入想搜尋的餐廳或分類'
+
+  if (keyword.length === 0) {
+    res.render('index', { notice: emptySearchNotice })
+    return
+  }
+
+  return Restaurant.find({
+    $or: [
+      { name: { $regex: `${keyword}`, $options: 'i' } },
+      { name_en: { $regex: `${keyword}`, $options: 'i' } },
+      { category: { $regex: `${keyword}`, $options: 'i' } }
+    ]
+  })
+    .lean()
+    .then(restaurants => {
+      if (restaurants.length === 0) {
+        res.render('index', { keyword: keyword, notice: noResultNotice })
+      } else {
+        res.render('index', { restaurants: restaurants, keyword: keyword })
+      }
+    })
+    .catch(error => console.log(error))
 })
 
 router.post('/', (req, res) => {
@@ -67,35 +96,6 @@ router.delete('/:id', (req, res) => {
   return Restaurant.findById(id)
     .then(restaurant => restaurant.remove())
     .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
-//Error
-router.get('/search', (req, res) => {
-  const keyword = req.query.keyword.trim()
-  const noResultNotice = `你搜尋的${keyword}沒有符合的餐廳`
-  const emptySearchNotice = '請輸入想搜尋的餐廳或分類'
-
-  if (keyword.length === 0) {
-    res.render('index', { notice: emptySearchNotice })
-    return
-  }
-
-  return Restaurant.find({
-    $or: [
-      { name: { $regex: `${keyword}`, $options: 'i' } },
-      { name_en: { $regex: `${keyword}`, $options: 'i' } },
-      { category: { $regex: `${keyword}`, $options: 'i' } }
-    ]
-  })
-    .lean()
-    .then(restaurants => {
-      if (restaurants.length === 0) {
-        res.render('index', { keyword: keyword, notice: noResultNotice })
-      } else {
-        res.render('index', { restaurants: restaurants, keyword: keyword })
-      }
-    })
     .catch(error => console.log(error))
 })
 
